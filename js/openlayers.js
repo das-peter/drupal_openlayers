5ac843e603ae0be5bc177fe6937300717d704377
Patrick Hayes <phayes@47098.no-reply.drupal.org>
2009-04-09 07:26:32 +0000

    comments, styling, features are now added to map from CCK

--- js/openlayers.js
+++ js/openlayers.js
@@ -4,22 +4,26 @@
  * When document is ready for JS
  */
 jQuery(document).ready(function() {
-  // Store rendered maps in Drupal.setting
+  // Store rendered maps and other OpenLayer objects in Drupal.openlayers.activeObjects
   Drupal.openlayers = {}
   Drupal.openlayers.activeObjects = [];
+  
   // Go through array and make maps
   for (var i in Drupal.settings.openlayers.maps) {
     var map = Drupal.settings.openlayers.maps[i];
-    // Check there is an ID for the map
+    // Check to see if there is a div on the page ready for the map. If there is then proceed.
     if ($('#' + map.id).length > 0) {
       // Make div the right dimensions and add custom controls
       $('#' + map.id).css('width', map.width).css('height', map.height);
       $('#' + map.id).after('<div class="openlayers-controls" id="openlayers-controls-' + map.id + '"></div>');
       $('#openlayers-controls-' + map.id).css("position","relative").css("bottom",map.height);
       
-      
-      //Set-up our registry of active OpenLayers javascript objects
+      // Set-up our registry of active OpenLayers javascript objects for this particular map.
       Drupal.openlayers.activeObjects[map.id] = {};
+      
+      // Set up places for us to store layers, controls, etc.
+      Drupal.openlayers.activeObjects[map.id].controls = [];
+      Drupal.openlayers.activeObjects[map.id].layers = [];
       
       // Render Map
       openlayersRenderMap(Drupal.settings.openlayers.maps[i]);
@@ -31,43 +35,44 @@ jQuery(document).ready(function() {
  * Render OpenLayers Map
  */
 function openlayersRenderMap(map) {
-		
   // Create base map options
   var options = openlayersCreateMapOptions(map.options, map.controls, map.id);
   
   // Store map in our registry of active OpenLayers objects
   Drupal.openlayers.activeObjects[map.id].map = new OpenLayers.Map(map.id, options);
     
-  // Add ID to map
+  // Add ID to map.
+  // @@TODO: Properly namespace this.
   Drupal.openlayers.activeObjects[map.id].map.mapid = map.id;
   
-  //Add events to the map 
+  // Add events to the map 
   for (var evtype in map.events){
-  	for (var ev in map.events[evtype]){ 
- 			eval("Drupal.openlayers.activeObjects[map.id].map.events.register(evtype,Drupal.openlayers.activeObjects[map.id].map," +  map.events[evtype][ev] + ");");  //Can we do this without using eval()?  		
-  	}
+    for (var ev in map.events[evtype]){ 
+      //@@TODO: Do this without eval. See http://drupal.org/node/172169 on why we should not use eval.
+      eval("Drupal.openlayers.activeObjects[map.id].map.events.register(evtype,Drupal.openlayers.activeObjects[map.id].map," +  map.events[evtype][ev] + ");");
+    }
   }
   
   // We set up all our layers
   openlayersProcessLayers(map.layers, map.id);
   
-  // Set up layers for editablity
+  // Make some layers editable and set-up the editing interface.
   if (map.draw_features) {
     openlayersProcessDrawFeatures(map.draw_features, map.id);
   }
   
   // Add layers to map
   for (var l in Drupal.openlayers.activeObjects[map.id].layers) {
-  	var layer =  Drupal.openlayers.activeObjects[map.id].layers[l];
-  	Drupal.openlayers.activeObjects[map.id].map.addLayer(layer);
+    var layer =  Drupal.openlayers.activeObjects[map.id].layers[l];
+    Drupal.openlayers.activeObjects[map.id].map.addLayer(layer);
   }
   
   // Add controls to map
   for (var c in Drupal.openlayers.activeObjects[map.id].controls) {
-  	var control = Drupal.openlayers.activeObjects[map.id].controls[c];
-  	Drupal.openlayers.activeObjects[map.id].map.addControl(control);
-  	if (control.activeByDefault) control.activate();
-	}
+    var control = Drupal.openlayers.activeObjects[map.id].controls[c];
+    Drupal.openlayers.activeObjects[map.id].map.addControl(control);
+    if (control.activeByDefault) control.activate();
+  }
                
   // Zoom to Center
   var center = new OpenLayers.LonLat(map.center.lon, map.center.lat);
@@ -99,75 +104,90 @@ function openlayersCreateMapOptions(options, controls, mapid) {
 }
 
 /**
- * Process Draw Features. This is for marking vector layers as editable. Calling this function will add standard functionality for adding and editing features.
+ * Process Draw Features. 
+ *
+ * This is for marking vector layers as editable. 
+ * This function will add standard functionality for adding and editing features.
+ * This function does no *do* anything with the features other than allow them to be drawn and edited by the interface. 
+ * Use featureadded_handler and featuremodified_handler if you wish to do something with the drawn/edited features.
  */
 function openlayersProcessDrawFeatures(drawFeatures, mapid) {
-  
-  //Set up a place to store our controls in our registry of active OpenLayers objects
-  Drupal.openlayers.activeObjects[mapid].controls = [];
-  
   // Add Base Pan button
-  // TODO: Make this out put a themed item in PHP
-  $('#openlayers-controls-' + mapid).append('<a href="#" id="openlayers-controls-pan-' + mapid + '" class="openlayers-controls-draw-feature-link openlayers-controls-draw-feature-link-pan openlayers-controls-draw-feature-link-on" rel="type:pan;mapid:'+mapid+'"></a>');
+  $('#openlayers-controls-' + mapid).append('<a href="#" id="openlayers-controls-pan-' + mapid + '" class="openlayers-controls-draw-feature-link openlayers-controls-draw-feature-link-pan openlayers-controls-draw-feature-link-on" rel="type:pan;mapid:' + mapid + '"></a>');
   
-  // Go through draw features
+  // Go through each type of feature (point, path, polygon) that is specified and build functionality
   for (var dF in drawFeatures) {
+  	// Get the OpenLayers layer object that will be editable.
     var layer = Drupal.openlayers.activeObjects[mapid].layers[drawFeatures[dF].vector];
     var typeLower = drawFeatures[dF].type.toLowerCase();
     
-    // Add control to list
+    // Determine what handler we need to use.
     if (drawFeatures[dF].type == 'Point')       var handler = OpenLayers.Handler.Point;
-    if (drawFeatures[dF].type == 'Path')  			var handler = OpenLayers.Handler.Path;
+    if (drawFeatures[dF].type == 'Path')        var handler = OpenLayers.Handler.Path;
     if (drawFeatures[dF].type == 'Polygon')     var handler = OpenLayers.Handler.Polygon;
-		
-		// Add our create and modify controls to the control object. Use a # prefix since these are speacial controls created by drawFeatures.
-    var create_control = new OpenLayers.Control.DrawFeature(layer, handler);
-    var modify_control = new OpenLayers.Control.ModifyFeature(layer);
+    
+    // Create our conrols and attach them to our layer.
+    var createControl = new OpenLayers.Control.DrawFeature(layer, handler);
+    var modifyControl = new OpenLayers.Control.ModifyFeature(layer);
     
     //Disable the active mode by default.  This could be changed if we wanted people to draw on the map immediately.
-    create_control.activeByDefault = false;
-    modify_control.activeByDefault = false;
+    createControl.activeByDefault = false;
+    modifyControl.activeByDefault = false;
     
-    create_control.drawType = typeLower;
-    modify_control.drawType = typeLower;
+    //Mark on the control what it is for drawing (point, path, or polygon)
+    createControl.drupalDrawType = typeLower;
+    modifyControl.drupalDrawType = typeLower;
     
-    Drupal.openlayers.activeObjects[mapid].controls['#create-' + typeLower] = create_control;
-    Drupal.openlayers.activeObjects[mapid].controls['#modify-' + typeLower] = modify_control;
+    // Add our create and modify controls to the controls object.
+    // Use a # prefix since these are special controls created by drawFeatures.
+    Drupal.openlayers.activeObjects[mapid].controls['#create-' + typeLower] = createControl;
+    Drupal.openlayers.activeObjects[mapid].controls['#modify-' + typeLower] = modifyControl;
             
-    // Add event handler
+    // Add special event handlers to controls
     if (drawFeatures[dF].featureadded_handler) {
-      eval("create_control.events.register('featureadded',create_control," + drawFeatures[dF].featureadded_handler + ");");  //Can we do this without using eval()?
+      for (var ev in drawFeatures[dF].featureadded_handler){
+      	  // @@TODO: Do this without eval. See http://drupal.org/node/172169 on why we should not use eval.
+          eval("createControl.events.register('featureadded',createControl," + drawFeatures[dF].featureadded_handler[ev] + ");");
+      }
+    }
+    if (drawFeatures[dF].featuremodified_handler) {
+    	for (var ev in drawFeatures[dF].featuremodified_handler){ 
+    		// @@TODO: Do this without eval.
+        eval("layer.events.register('afterfeaturemodified',layer," + drawFeatures[dF].featuremodified_handler[ev] + ");");
+      }
     }
         
-    // Add action link
-    // TODO: Make this out put a themed item in PHP, might need a placeholder for dF
-    $('#openlayers-controls-' + mapid).append('<a href="#" id="openlayers-controls-draw-' + typeLower + '-' + mapid + '" class="openlayers-controls-draw-feature-link openlayers-controls-draw-feature-link-' + typeLower + ' openlayers-controls-draw-feature-link-off" rel="type:' + dF + ';mapid:'+ mapid +'"></a>');
+    // Add action link (button)
+    // We store the type and associated mapid in the rel attribute.
+    $('#openlayers-controls-' + mapid).append('<a href="#" id="openlayers-controls-draw-' + typeLower + '-' + mapid + '" class="openlayers-controls-draw-feature-link openlayers-controls-draw-feature-link-' + typeLower + ' openlayers-controls-draw-feature-link-off" rel="type:' + dF + ';mapid:' + mapid + '"></a>');
   }
   
-  // Add click event
+  // Add click event to the action link (button)
   $('.openlayers-controls-draw-feature-link').click(
     function() {
+    	// Grab the mapid and the type from the rel attribute.
       var parsedRel = openlayersParseRel($(this).attr('rel'));
       
-      // @@TODO put in scope of map container.  Currently turning off all on page.
+      // Change the look of the action link
       $('.openlayers-controls-draw-feature-link').removeClass('openlayers-controls-draw-feature-link-on');
       $('.openlayers-controls-draw-feature-link').addClass('openlayers-controls-draw-feature-link-off');
       $(this).addClass('openlayers-controls-draw-feature-link-on');
       $(this).removeClass('openlayers-controls-draw-feature-link-off');
       
+      // Cycle through the different possible types of controls (polygon, line, point, pan)
       for (var f in Drupal.settings.openlayers.maps[parsedRel['mapid']].draw_features){
-      	var createControl = Drupal.openlayers.activeObjects[parsedRel['mapid']].controls['#create-'+f];
-      	var modifyControl = Drupal.openlayers.activeObjects[parsedRel['mapid']].controls['#modify-'+f];
-      	
-      	//Deactivate everything
-      	createControl.deactivate();
-      	modifyControl.deactivate();
-      		
-      	//Activate it if it matches
-      	if (parsedRel['type'] == createControl.drawType){
-      		createControl.activate();
-      		modifyControl.activate();
-      	}
+        var createControl = Drupal.openlayers.activeObjects[parsedRel['mapid']].controls['#create-' + f];
+        var modifyControl = Drupal.openlayers.activeObjects[parsedRel['mapid']].controls['#modify-' + f];
+        
+        // Deactivate everything
+        createControl.deactivate();
+        modifyControl.deactivate();
+          
+        // Activate it if it matches
+        if (parsedRel['type'] == createControl.drupalDrawType){
+          createControl.activate();
+          modifyControl.activate();
+        }
       }
       return false;
     }
@@ -175,41 +195,35 @@ function openlayersProcessDrawFeatures(drawFeatures, mapid) {
 }
 
 /**
- * Default Feature Handler
- */
-function openlayersDefaultFeatureHandler(feature) {
-  // Do something.
-}
-
-/**
  * Process Layers
  */
 function openlayersProcessLayers(layers, mapid) {
-	
-	Drupal.openlayers.activeObjects[mapid].layers = [];
-	
+  
+  Drupal.openlayers.activeObjects[mapid].layers = [];
+  
   // Go through layers
   if (layers) {
     for (var layer in layers) {
       switch (layers[layer].type) {
         case 'WMS':    
-        	var new_layer = openlayersProcessLayerWMS(layers[layer], mapid);
-        	Drupal.openlayers.activeObjects[mapid].layers[layer] = new_layer;
+          var newLayer = openlayersProcessLayerWMS(layers[layer], mapid);
+          Drupal.openlayers.activeObjects[mapid].layers[layer] = newLayer;
           break;    
         case 'Vector':
-        	var new_layer = openlayersProcessLayerVector(layers[layer], mapid);
-       		Drupal.openlayers.activeObjects[mapid].layers[layer] = new_layer;
+          var newLayer = openlayersProcessLayerVector(layers[layer], mapid);
+           Drupal.openlayers.activeObjects[mapid].layers[layer] = newLayer;
           break;
       }
-      //Add our Drupal data to the layer
-      new_layer.drupalId = layer;
-      new_layer.drupalData = layers[layer];
+      // Add our Drupal data to the layer
+      newLayer.drupalId = layer;
+      newLayer.drupalData = layers[layer];
       
-      //Add events
+      // Add events
       for (var evtype in layers[layer].events){
-      	for (var ev in layers[layer].events[evtype]){ 
-      		eval("new_layer.events.register(evtype,new_layer," + layers[layer].events[evtype][ev] + ");");  //Can we do this without using eval()?  		
-      	}
+        for (var ev in layers[layer].events[evtype]){ 
+        	//@@TODO: Do this without eval. See http://drupal.org/node/172169 on why we should not use eval.
+          eval("new_layer.events.register(evtype,new_layer," + layers[layer].events[evtype][ev] + ");");
+        }
       }
     }
   }
@@ -264,24 +278,24 @@ function openlayersProcessLayerVector(layerOptions, mapid) {
  * Parse out key / value pairs out of a string that looks like "key:value;key2:value2"
  */
 function openlayersParseRel(rel){
-	var outputArray = [];
-	
-	//Some preprosessing
-	//replace dangling whitespaces. Use regex?
-	rel = rel.replace('; ',';');
-	//Cut out final ; if it exists
-	if (rel.charAt(rel.length-1) == ";") rel = rel.substr(0,rel.length-1);
-	
-	//Get all the key:value strings
-	var keyValueStrings = rel.split(';');
-	
-	//Process the key:value strings into key:value pairs
-	for (var i in keyValueStrings){
-		var singleKeyValue = keyValueStrings[i].split(':');
-		outputArray[singleKeyValue[0]] = singleKeyValue[1];
-	}
-	
-	return outputArray;
+  var outputArray = [];
+  
+  // Some preprosessing
+  // replace dangling whitespaces. Use regex?
+  rel = rel.replace('; ',';');
+  //Cut out final ; if it exists
+  if (rel.charAt(rel.length-1) == ";") rel = rel.substr(0,rel.length-1);
+  
+  //Get all the key:value strings
+  var keyValueStrings = rel.split(';');
+  
+  // Process the key:value strings into key:value pairs
+  for (var i in keyValueStrings){
+    var singleKeyValue = keyValueStrings[i].split(':');
+    outputArray[singleKeyValue[0]] = singleKeyValue[1];
+  }
+  
+  return outputArray;
 }
 
 /**
@@ -295,7 +309,7 @@ function openlayersVarDump(element, limit, depth) {
   for (property in element) {
     //Property domConfig isn't accessable
     if (property != 'domConfig') {
-      returnString += '<li><strong>'+ property + '</strong> <small>(' + (typeof element[property]) +')</small>';
+      returnString += '<li><strong>'+ property + '</strong> <small>(' + (typeof element[property]) + ')</small>';
       if (typeof element[property] == 'number' || typeof element[property] == 'boolean')
         returnString += ' : <em>' + element[property] + '</em>';
       if (typeof element[property] == 'string' && element[property])
@@ -309,7 +323,7 @@ function openlayersVarDump(element, limit, depth) {
   returnString += '</ol>';
   if (depth == 0) {
     winpop = window.open("", "","width=800,height=600,scrollbars,resizable");
-    winpop.document.write('<pre>'+returnString+ '</pre>');
+    winpop.document.write('<pre>' + returnString + '</pre>');
     winpop.document.close();
   }
   return returnString;
--- modules/openlayers_cck/js/openlayers_cck.js
+++ modules/openlayers_cck/js/openlayers_cck.js
@@ -8,6 +8,9 @@
 
 /**
  * When document is ready for JS
+ * 
+ * Add the themed map container (HTML) to the document. 
+ * Also set-up the click functionality for the Show/Hide WKT Fields
  */
 jQuery(document).ready(function() {
   // Go through CCK Fields and diplay map
@@ -18,14 +21,10 @@ jQuery(document).ready(function() {
     $('#' + fieldContainer).before(Drupal.settings.openlayers_cck.maps[map].field_map_themed);
     $('#' + fieldContainer).hide();
     
-    // Get default features
-    // @@TODO: not sure how to do this?	
-    // openlayersCCKPopulateMap(Drupal.settings.openlayers_cck.maps[map].field_items, vector)
-    
     // Define click actions for WKT Switcher
     $('#' + map + '-wkt-switcher').click(function() {
-    	var mapid = $(this).attr('rel');
-    	var fieldContainer = Drupal.settings.openlayers_cck.maps[mapid].field_container;
+      var mapid = $(this).attr('rel');
+      var fieldContainer = Drupal.settings.openlayers_cck.maps[mapid].field_container;
       $('#' + fieldContainer).toggle();
       return false;
     });
@@ -33,16 +32,17 @@ jQuery(document).ready(function() {
 });
 
 /**
- * OpenLayers CCK Feature Handler
+ * OpenLayers CCK Feature Added Handler
+ * 
+ * This function is triggered when a feature is added by the user
  */
-function openlayersCCKFeatureHandler(event) {
-	
-	var feature = event.feature;
-	
+function openlayersCCKFeatureAdded(event) {
+	//Get the feature we have just added out of the event object
+  var feature = event.feature;
+  
   // Make some variables
   var featureCount = feature.layer.features.length;
   var featureNew = featureCount - 1;
-  
   var mapid = feature.layer.map.mapid;
   
   // Get field names
@@ -53,11 +53,12 @@ function openlayersCCKFeatureHandler(event) {
     }
   }
   
+  // This is the id of the textfield we will be assigning this feature to.
   var wktFieldNewID = 'edit-' + fieldName + '-' + featureNew + '-wkt';
+  // This is the "Add another item" button
   var wktFieldAddID = 'edit-' + fieldName + '-' + fieldName + '-add-more';
   
-  // If new features, add field to feature
-    
+  // Clone the geometry so we may safetly work on it without hurting the feature
   var geometry = feature.geometry.clone();
     
   // Assign field to feature
@@ -65,33 +66,43 @@ function openlayersCCKFeatureHandler(event) {
   // geometry.transform(map_proj, maps[field_name]['dbproj']);
   var wkt = geometry.toString();
   $('#' + wktFieldNewID).val(wkt);
-  
-  // If delete, remove value form field
-  
+    
   // Add another field ..
-  // @@TODO:  When reloading this function is reloading all of Drupal.settings.openlayers  .  On the reload for some reason it is only reloading our current CCK-field, not all CCK-fields, and so is causing the error: Drupal.settings.openlayers.maps[parsedRel.mapid] is undefined
+  // @@BUG:  When triggering this function it is reloading all of Drupal.settings.openlayers.  On the reload for some reason it is only reloading our current CCK-field, not all CCK-fields, and so is causing the error: Drupal.settings.openlayers.maps[parsedRel.mapid] is undefined
   $('#' + wktFieldAddID).trigger('mousedown');
 }
 
 /**
- * Get Values From CCK
+ * Get Values From CCK and populate the map with shapes
  */
-function openlayersCCKPopulateMap(features, vector){
+function openlayersCCKPopulateMap(mapid){  
   var featuresToAdd = [];
   var wktFormat = new OpenLayers.Format.WKT();
+  var fieldContainer = Drupal.settings.openlayers_cck.maps[mapid].field_container;
   
-  // Get Features
-  for (var f in features) {
-    if (features[f].wkt) {
-      var wkt = wktFormat.read(features[f].wkt);
-      // wkt.geometry.transform(maps[field_name]['dbproj'], map_proj);
-      featuresToAdd.push(wkt);
+  // Cycle through the fieldContainer item and read WKT from all the textareas
+  $('#' + fieldContainer + ' textarea').each(function(){
+    if ($(this).val() != ''){
+    	//read the wkt values into an OpenLayers geometry object
+      var newFeature = wktFormat.read($(this).val());
+      if (typeof(newFeature) == "undefined"){
+        alert(Drupal.t('WKT is not valid'));
+      }
+      else{
+      	// @@TODO: project the geometry if our map has a different geospatial projection as our CCK geo data.
+        // newFeature.geometry.transform(maps[field_name]['dbproj'], map_proj);
+        
+        // Link the feature to the field.
+        newFeature.drupalField = $(this).attr('id');
+        // Queue the feature for addition to the layer.
+        featuresToAdd.push(newFeature);
+      }
     }
-  }
+  });
   
   // Add features to vector
   if (featuresToAdd.length != 0){
-    vector.addFeatures(featuresToAdd);
+    Drupal.openlayers.activeObjects[mapid].layers['default_vector'].addFeatures(featuresToAdd);
   }
 }
 
@@ -100,7 +111,19 @@ function openlayersCCKPopulateMap(features, vector){
  * When the layer is done loading, load in the values from the CCK text fields if it is the correct layer.
  */
 function openlayersCCKLoadValues(event){
-	if (event.layer.drupalId == "default_vector"){
-		//Do something with openlayersCCKPopulateMap
-	}
+  if (event.layer.drupalId == "default_vector"){
+    openlayersCCKPopulateMap(event.layer.map.mapid);
+  }
+}
+
+
+/**
+ * When the any feature on the layer is modified, fill in the WKT values into the text fields
+ */
+function openlayersCCKFeatureModified(event){
+  
+  // @@TODO: If modified, update CCK fields
+  
+  // @@TODO: If deleted, remove value form field
+  
 }
