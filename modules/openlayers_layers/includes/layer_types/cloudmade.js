ff4dc51b509f9020805ae9a6529e0a37056ec860
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2009-11-10 22:16:42 +0000

    Working CloudMade layer

--- modules/openlayers_layers/includes/layer_types/cloudmade.js
+++ modules/openlayers_layers/includes/layer_types/cloudmade.js
@@ -11,8 +11,9 @@
 Drupal.openlayers.layer.cloudmade = function (name, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
 
-  options.sphericalMercator = true;
+  // options.sphericalMercator = true;
   options.maxExtent = new OpenLayers.Bounds(-20037508.34,-20037508.34,20037508.34,20037508.34);
+  options.projection = new OpenLayers.Projection('EPSG:'+options.projection);
 
   var layer = new OpenLayers.Layer.CloudMade(name, options);
   layer.styleMap = styleMap;
