09b00d41829c521b87e70741ae94f82565667f50
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2009-11-30 23:04:10 +0000

    Patch from #647126, thanks yhahn. Allows correct setting of parameters in OpenLayers Vector layer constructor

--- modules/openlayers_views/includes/layer_types/openlayers_views_vector.js
+++ modules/openlayers_views/includes/layer_types/openlayers_views_vector.js
@@ -1,9 +1,8 @@
 Drupal.openlayers.layer.openlayers_views_vector = function (name, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
     options.projection = new OpenLayers.Projection('EPSG:'+options.projection);
-    //layer.styleMap = styleMap;
-    var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
-    var layer = new OpenLayers.Layer.Vector(name, {'styleMap': styleMap});
+    options.options.styleMap = Drupal.openlayers.getStyleMap(map, options.name);
+    var layer = new OpenLayers.Layer.Vector(name, options.options);
     if (options.features) {
       Drupal.openlayers.addFeatures(map, layer, options.features);
     }
