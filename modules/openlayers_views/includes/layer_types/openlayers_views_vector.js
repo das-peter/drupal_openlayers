a314ffd46bbf122726a3f9df36476494d4f397d1
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-01-27 21:02:55 +0000

    Fixing openlayers_views layer type

--- modules/openlayers_views/includes/layer_types/openlayers_views_vector.js
+++ modules/openlayers_views/includes/layer_types/openlayers_views_vector.js
@@ -1,10 +1,9 @@
 Drupal.openlayers.layer.openlayers_views_vector = function (name, map, options) {
-  var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
-    options.projection = new OpenLayers.Projection('EPSG:'+options.projection);
-    options.options.styleMap = Drupal.openlayers.getStyleMap(map, options.name);
-    var layer = new OpenLayers.Layer.Vector(name, options.options);
-    if (options.features) {
-      Drupal.openlayers.addFeatures(map, layer, options.features);
-    }
-    return layer;
+  options.projection = new OpenLayers.Projection('EPSG:'+options.projection);
+  options.options.styleMap = Drupal.openlayers.getStyleMap(map, name);
+  var layer = new OpenLayers.Layer.Vector(name, options.options);
+  if (options.features) {
+    Drupal.openlayers.addFeatures(map, layer, options.features);
+  }
+  return layer;
 };
