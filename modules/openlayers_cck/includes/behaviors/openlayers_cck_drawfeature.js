7c77c1ceb06e61f69998d7d1ec3f35a8824c9e59
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-02-01 18:55:29 +0000

    Adding openlayers_cck_drawfeature behavior code

---
+++ modules/openlayers_cck/includes/behaviors/openlayers_cck_drawfeature.js
@@ -0,0 +1,43 @@
+// $Id$
+
+/**
+ * @file
+ * Main JS file for openlayers_cck
+ *
+ * @ingroup openlayers_cck
+ */
+
+// Declare global variable
+openlayers_cck_drawfeature_wkt_field = null;
+
+function update(features) {
+  WktWriter = new OpenLayers.Format.WKT();
+  wkt_value = WktWriter.write(features.object.features);
+  openlayers_cck_drawfeature_wkt_field.text(wkt_value);
+}
+
+Drupal.behaviors.openlayers_cck_drawfeature = function(context) {
+  var data = $(context).data('openlayers');
+  if (data && data.map.behaviors['openlayers_cck_drawfeature']) {
+    // Add control
+    openlayers_cck_drawfeature_wkt_field = 
+      $("#"+data.map.behaviors['openlayers_cck_drawfeature'].wkt_field_id);
+
+    var data_layer = new OpenLayers.Layer.Vector("Data Layer",
+      {
+        eventListeners: {
+          "featureadded": update,
+          "featureremoved": update,
+          "featuremodified": update
+        }
+      }
+    );
+
+
+    data.openlayers.addLayer(data_layer);
+    
+    var control = new OpenLayers.Control.EditingToolbar(data_layer);
+    data.openlayers.addControl(control);
+    control.activate();
+  }
+};
