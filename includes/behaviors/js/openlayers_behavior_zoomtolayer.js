c6c10afd9245ad95afe849cc35f9f6291f86c98b
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-02-05 22:11:39 +0000

    Improving the zoomtolayers behavior

--- includes/behaviors/js/openlayers_behavior_zoomtolayer.js
+++ includes/behaviors/js/openlayers_behavior_zoomtolayer.js
@@ -8,16 +8,18 @@ Drupal.behaviors.openlayers_zoomtolayer = function(context) {
   if (data && data.map.behaviors['openlayers_behavior_zoomtolayer']) {
     map = data.openlayers;
     // TODO: just select layers you want, instead of all vector layers
-    layers = map.getLayersByClass('OpenLayers.Layer.Vector');
+    layers = map.getLayersByName(data.map.behaviors['openlayers_behavior_zoomtolayer'].zoomtolayer);
     for (var i in layers) {
-      if (layers[i].features.length > 1) {
+      if (layers[i].features !== undefined && layers[i].features.length > 1) {
         layerextent = layers[i].getDataExtent();
         map.zoomToExtent(layerextent);
       }
       else {
         layerextent = layers[i].getDataExtent();
-        map.zoomToExtent(layerextent);
-        map.zoomTo(data.map.behaviors['openlayers_behavior_zoomtolayer'].point_zoom_level);
+        if (layerextent !== undefined) {
+          map.zoomToExtent(layerextent);
+          map.zoomTo(data.map.behaviors['openlayers_behavior_zoomtolayer'].point_zoom_level);
+        }
       }
     }
   }
