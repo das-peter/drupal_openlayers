1572f561f5129239e1de1bcafa676818b8254a4b
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-02-22 01:59:28 +0000

    Rudimentary CCK display support

---
+++ modules/openlayers_cck/includes/behaviors/openlayers_cck_vector_layer.js
@@ -0,0 +1,19 @@
+// $Id$
+
+/**
+ * @file
+ * Main JS file for openlayers_cck
+ *
+ * @ingroup openlayers_cck
+ */
+
+Drupal.behaviors.openlayers_cck_vector_layer = function(context) {
+  var data = $(context).data('openlayers');
+  if (data && data.map.behaviors['openlayers_cck_vector_layer']) {
+    features = data.map.behaviors['openlayers_cck_vector_layer'].features;
+
+    var data_layer = new OpenLayers.Layer.Vector("Data Layer");
+    Drupal.openlayers.addFeatures(data.map, data_layer, features);
+    data.openlayers.addLayer(data_layer);
+  }
+};
