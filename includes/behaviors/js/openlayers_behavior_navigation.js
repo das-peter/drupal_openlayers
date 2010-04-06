50b344db19472cc0c27501b920b90de7b83172a9
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-04-06 15:34:38 +0000

    Adding zoomWheelEnabled option to Navigation control

--- includes/behaviors/js/openlayers_behavior_navigation.js
+++ includes/behaviors/js/openlayers_behavior_navigation.js
@@ -12,7 +12,10 @@ Drupal.behaviors.openlayers_behavior_navigation = function(context) {
   var data = $(context).data('openlayers');
   if (data && data.map.behaviors['openlayers_behavior_navigation']) {
     // Add control
-    var control = new OpenLayers.Control.Navigation();
+    var options = {
+      'zoomWheelEnabled': data.map.behaviors['openlayers_behavior_navigation'].zoomWheelEnabled
+    };
+    var control = new OpenLayers.Control.Navigation(options);
     data.openlayers.addControl(control);
     control.activate();
   }
