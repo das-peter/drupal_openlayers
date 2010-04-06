8d8fe203af49f15ef42d3b0f96207ce21d866cfc
Alan Palazzolo <drupal-git@zzolo.org>
2010-04-06 21:20:32 +0000

    #735172: Abstracted draw features behavior from CCK module.

--- includes/behaviors/js/openlayers_behavior_drawfeatures.js
+++ includes/behaviors/js/openlayers_behavior_drawfeatures.js
@@ -8,8 +8,13 @@
  */
 
 // Declare global variable
-openlayers_cck_drawfeature_wkt_field = null;
+openlayers_drawfeature_element = null;
 
+/**
+ * Update function for features
+ *
+ * TODO: put in a non-global namespace
+ */
 function update(features) {
   WktWriter = new OpenLayers.Format.WKT();
   for(var i in features.object.features) {
@@ -20,27 +25,32 @@ function update(features) {
     );
   }
   wkt_value = WktWriter.write(features.object.features);
-  openlayers_cck_drawfeature_wkt_field.text(wkt_value);
+  openlayers_drawfeature_element.text(wkt_value);
 }
 
-Drupal.behaviors.openlayers_cck_drawfeature = function(context) {
+/**
+ * Behavior for Draw Features
+ */
+Drupal.behaviors.openlayers_behavior_drawfeatures = function(context) {
   var data = $(context).data('openlayers');
-  if (data && data.map.behaviors['openlayers_cck_drawfeature']) {
+  if (data && data.map.behaviors['openlayers_behavior_drawfeatures']) {
+    var feature_types = data.map.behaviors['openlayers_behavior_drawfeatures'].feature_types;
+      
     // Add control
-    openlayers_cck_drawfeature_wkt_field = 
-      $("#"+data.map.behaviors['openlayers_cck_drawfeature'].wkt_field_id);
+    openlayers_drawfeature_element = 
+      $("#" + data.map.behaviors['openlayers_behavior_drawfeatures'].element_id);
 
     // Create options
     var options = {
       projection: new OpenLayers.Projection('EPSG:4326'),
-      drupalID: 'openlayers_cck_vector_layer'
+      drupalID: 'openlayers_drawfeatures_layer'
     };
-    var data_layer = new OpenLayers.Layer.Vector(Drupal.t("Data Layer"), options);
+    var data_layer = new OpenLayers.Layer.Vector(Drupal.t("Feature Layer"), options);
     data.openlayers.addLayer(data_layer);
 
-    if (openlayers_cck_drawfeature_wkt_field.text() != '') {
+    if (openlayers_drawfeature_element.text() != '') {
       var wktFormat = new OpenLayers.Format.WKT();
-      wkt = openlayers_cck_drawfeature_wkt_field.text();
+      wkt = openlayers_drawfeature_element.text();
       features = wktFormat.read(wkt);
       for(var i in features) {
         features[i].geometry = features[i].geometry.transform(
@@ -66,9 +76,6 @@ Drupal.behaviors.openlayers_cck_drawfeature = function(context) {
       'path': 'OpenLayers.Handler.Path',
       'polygon': 'OpenLayers.Handler.Polygon',
     };
-
-    var feature_types = 
-      data.map.behaviors['openlayers_cck_drawfeature'].feature_types;
 
     var c = [];
     for(var i in control.controls) {
