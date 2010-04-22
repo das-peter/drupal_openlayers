9f57406ca8c9d15aa3539668a034f6137ee8ebc9
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-04-22 13:08:25 +0000

    Removing extra logic in layer types

--- includes/layer_types/wms.js
+++ includes/layer_types/wms.js
@@ -10,9 +10,6 @@
  */
 Drupal.openlayers.layer.wms = function (title, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
-  if (options.type === undefined){
-    options.type = "image/png";
-  }
   options.options.transparent = true;
   var layer = new OpenLayers.Layer.WMS(title, 
     options.base_url, options.options, options.params);
