a758682c67b755a0616abb90188c1af5253c02dc
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-04-30 19:34:05 +0000

    Fixing drupal IDs on wms layers...

--- includes/layer_types/wms.js
+++ includes/layer_types/wms.js
@@ -11,6 +11,7 @@
 Drupal.openlayers.layer.wms = function (title, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
   options.options.transparent = true;
+  options.params.drupalID = options.drupalID;
   var layer = new OpenLayers.Layer.WMS(title, 
     options.base_url, options.options, options.params);
   layer.styleMap = styleMap;
