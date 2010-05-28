88629b16a5854cdfbdc484ed21afda8d6b93b900
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-05-28 12:20:11 +0000

    Adding buffer and ratio options for WMS layers, fixing [#810846], thanks strk

--- includes/layer_types/wms.js
+++ includes/layer_types/wms.js
@@ -10,6 +10,11 @@
  */
 Drupal.openlayers.layer.wms = function (title, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
+
+  /* TODO: have PHP take care of the casts here, not JS! */
+  options.params.buffer = parseInt(options.params.buffer, 10);
+  options.params.ratio = parseFloat(options.params.ratio);
+
   options.params.drupalID = options.drupalID;
   var layer = new OpenLayers.Layer.WMS(title, 
     options.base_url, options.options, options.params);
