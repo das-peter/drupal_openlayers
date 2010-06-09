33c307768f11c45f8010cceb77cc15b8306a5931
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-06-09 12:57:17 +0000

    Fixing [#784092] partially - this might require a little more to be completely working

--- includes/layer_types/js/wms.js
+++ includes/layer_types/js/wms.js
@@ -12,8 +12,12 @@ Drupal.openlayers.layer.wms = function (title, map, options) {
   var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
 
   /* TODO: have PHP take care of the casts here, not JS! */
-  options.params.buffer = parseInt(options.params.buffer, 10);
-  options.params.ratio = parseFloat(options.params.ratio);
+  if (options.params.buffer) {
+    options.params.buffer = parseInt(options.params.buffer, 10);
+  }
+  if (options.params.ratio) {
+    options.params.ratio = parseFloat(options.params.ratio);
+  }
 
   options.params.drupalID = options.drupalID;
   var layer = new OpenLayers.Layer.WMS(title, 
