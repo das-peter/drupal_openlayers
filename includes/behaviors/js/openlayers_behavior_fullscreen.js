d4c466ce6b63a1023680db0537576f6afff8752c
Alan Palazzolo <drupal-git@zzolo.org>
2010-07-18 22:10:44 +0000

    #854928 by strk: Added activate by default option for fullscreen behavior.

--- includes/behaviors/js/openlayers_behavior_fullscreen.js
+++ includes/behaviors/js/openlayers_behavior_fullscreen.js
@@ -4,32 +4,57 @@
  * @file
  * JS Implementation of OpenLayers behavior.
  */
+ 
+/**
+ * Global variables to help with scope
+ *
+ * TODO: Move this to a better place, like the map data().
+ */
+Drupal.openlayers = Drupal.openlayers || {};
+Drupal.openlayers.fullscreen = Drupal.openlayers.fullscreen || {};
 
 /**
  * Attribution Behavior
  */
 Drupal.behaviors.openlayers_behavior_fullscreen = function(context) {
-  var fullscreen_panel, data = $(context).data('openlayers');
+  var fullscreenPanel;
+  var data = $(context).data('openlayers');
   if (data && data.map.behaviors['openlayers_behavior_fullscreen']) {
-    fullscreen_panel = new OpenLayers.Control.Panel(
+    var opts = data.map.behaviors['openlayers_behavior_fullscreen'];
+    
+    // Create new panel control and add.
+    fullscreenPanel = new OpenLayers.Control.Panel(
       {
-        displayClass: "openlayers_behavior_fullscreen_button_panel"
+        displayClass: 'openlayers_behavior_fullscreen_button_panel'
       }
     );
-    data.openlayers.addControl(fullscreen_panel);
+    data.openlayers.addControl(fullscreenPanel);
+    
+    // Create toggleing control and cutton.
+    var toggler = OpenLayers.Function.bind(
+      Drupal.openlayers.fullscreen.fullscreenToggle, data);
     var button = new OpenLayers.Control.Button({
-      displayClass: "openlayers_behavior_fullscreen_button", 
-      title: "Fullscreen", 
-      trigger: openlayers_behavior_fullscreen_toggle
+      displayClass: 'openlayers_behavior_fullscreen_button', 
+      title: Drupal.t('Fullscreen'), 
+      trigger: toggler
     });
-    fullscreen_panel.addControls([button]);
+    fullscreenPanel.addControls([button]);
+    
+    // Make fullscreen by default if activited.
+    if (opts['activated'] == true) {
+      toggler();
+    }
   }
 }
 
-openlayers_behavior_fullscreen_toggle = function(context) {
-  $map = $(this.map.div);
-  extent = $map.data('openlayers').openlayers.getExtent();
-  
+/**
+ * Toggling function for FullScreen control.
+ */
+Drupal.openlayers.fullscreen.fullscreenToggle = function() {
+  var map = this.openlayers;
+  var $map = $(this.openlayers.div);
+  var extent = map.getExtent();
+
   $map.parent().toggleClass('openlayers_map_fullscreen');
   $map.toggleClass('openlayers_map_fullscreen');
   $map.data('openlayers').openlayers.updateSize();
