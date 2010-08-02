1e9e6cd80a62e8f8f5c57d85cf491f82642d2d77
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2010-08-02 21:20:28 +0000

    [872008] by davideads: assigning stylemaps to raw layers

--- includes/layer_types/js/raw.js
+++ includes/layer_types/js/raw.js
@@ -4,15 +4,18 @@
  * OpenLayers Raw Layer Handler
  */
 Drupal.openlayers.layer.openlayers_raw = function(title, map, options) {
+  options.options = options.options || {}
+
   // Note, so that we do not pass all the features along to the Layer
   // options, we use the options.options to give to Layer
   // options.options.drupalID = options.drupalID;
   var layer = new OpenLayers.Layer.Vector(title, options.options);
-  
-  // Add fetures if there are any
+
+  options.options.styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
+
   if (options.features) {
     Drupal.openlayers.addFeatures(map, layer, options.features);
   }
-  
+
   return layer;
 };
