0ea4d8fdc586e79ecf6aaeddefdbdf611ab81bc8
strk <strk@781486.no-reply.drupal.org>
2010-08-30 16:09:49 +0000

    #897560 : add 'descending' parameter to layer switcher

--- includes/behaviors/js/openlayers_behavior_layerswitcher.js
+++ includes/behaviors/js/openlayers_behavior_layerswitcher.js
@@ -12,7 +12,9 @@ Drupal.behaviors.openlayers_behavior_layerswitcher = function(context) {
   var data = $(context).data('openlayers');
   if (data && data.map.behaviors['openlayers_behavior_layerswitcher']) {
     // Add control
-    var control = new OpenLayers.Control.LayerSwitcher();
+    var control = new OpenLayers.Control.LayerSwitcher({
+      'ascending': !data.map.behaviors['openlayers_behavior_layerswitcher'].descending
+    });
     data.openlayers.addControl(control);
     control.activate();
   }
