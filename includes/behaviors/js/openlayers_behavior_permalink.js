965d8c8c073f0f5f1f6c8d0874b2b7da54114f6f
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2011-01-21 17:50:04 +0000

    #1035458 by tmcw: fixing included javascript file syntax errors.

--- includes/behaviors/js/openlayers_behavior_permalink.js
+++ includes/behaviors/js/openlayers_behavior_permalink.js
@@ -10,13 +10,14 @@
  */
 (function($) {
   Drupal.behaviors.openlayers_behavior_permalink = {
-    'attach': function(context, settings) {
-    var data = $(context).data('openlayers');
-    if (data && data.map.behaviors['openlayers_behavior_permalink']) {
-      // Add control
-      var control = new OpenLayers.Control.Permalink();
-      data.openlayers.addControl(control);
-      control.activate();
+      'attach': function(context, settings) {
+      var data = $(context).data('openlayers');
+      if (data && data.map.behaviors['openlayers_behavior_permalink']) {
+        // Add control
+        var control = new OpenLayers.Control.Permalink();
+        data.openlayers.addControl(control);
+        control.activate();
+      }
     }
   }
 })(jQuery);
--- includes/behaviors/js/openlayers_behavior_scaleline.js
+++ includes/behaviors/js/openlayers_behavior_scaleline.js
@@ -11,12 +11,13 @@
 (function($) {
 Drupal.behaviors.openlayers_behavior_scaleline = {
   'attach': function(context, settings) {
-  var data = $(context).data('openlayers');
-  if (data && data.map.behaviors['openlayers_behavior_scaleline']) {
-    // Add control
-    var control = new OpenLayers.Control.ScaleLine();
-    data.openlayers.addControl(control);
-    control.activate();
+    var data = $(context).data('openlayers');
+    if (data && data.map.behaviors['openlayers_behavior_scaleline']) {
+      // Add control
+      var control = new OpenLayers.Control.ScaleLine();
+      data.openlayers.addControl(control);
+      control.activate();
+    }
   }
 }
 })(jQuery);
