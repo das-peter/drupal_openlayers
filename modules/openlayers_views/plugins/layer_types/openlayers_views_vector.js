48a7393e965d984859ea3e77d18c7c62ca2648ed
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2012-08-03 17:18:03 +0200

    Miscellaneous fixes, cleanup, todos.

--- modules/openlayers_views/plugins/layer_types/openlayers_views_vector.js
+++ modules/openlayers_views/plugins/layer_types/openlayers_views_vector.js
@@ -14,7 +14,7 @@ Drupal.openlayers.layer.openlayers_views_vector = function(title, map, options) 
   // Create layer object
   var layer = new OpenLayers.Layer.Vector(title, options.options);
 
-  // Add fetures if there are any
+  // Add features if there are any
   if (options.features) {
     Drupal.openlayers.addFeatures(map, layer, options.features);
   }
