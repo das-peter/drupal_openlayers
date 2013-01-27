b7c01b7db516eb244d63499372989436f1c3f383
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-01-27 21:52:06 +0100

    Removed console.log.

--- js/openlayers.js
+++ js/openlayers.js
@@ -177,12 +177,6 @@ Drupal.openlayers = {
       // Ensure that the layer handler is available
       if (options.layer_handler !== undefined &&
         Drupal.openlayers.layer[options.layer_handler] !== undefined) {
-
-        console.log('map.layers[name]');
-        console.log(map.layers[name]);
-        console.log('options');
-        console.log(options);
-
         var layer = Drupal.openlayers.layer[options.layer_handler](map.layers[name].title, map, options);
 
         layer.visibility = !!(!map.layer_activated || map.layer_activated[name]);
