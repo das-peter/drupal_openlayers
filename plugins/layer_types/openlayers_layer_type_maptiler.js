0d93a7a079d1c2c081f2bdc61ad980cbbf615465
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-03-21 22:25:05 +0100

    Make sure that 'resolutions' and 'maxExtent' are float value instead of strings. MapTiler layer type should work now.

--- plugins/layer_types/openlayers_layer_type_maptiler.js
+++ plugins/layer_types/openlayers_layer_type_maptiler.js
@@ -11,14 +11,19 @@ Drupal.openlayers.layer.maptiler = function(title, map, options) {
   if (options.maxExtent !== undefined) {
     options.maxExtent = new OpenLayers.Bounds.fromArray(options.maxExtent);
   }
+
   options.getURL = function(bounds) {
     var res = this.map.getResolution();
     var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
     var y = Math.round((bounds.bottom - this.tileOrigin.lat) / (res * this.tileSize.h));
     var z = this.map.getZoom();
-    return this.url + z + '/' + x + '/' + y + '.' + this.type;
+    if (x >= 0 && y >= 0) {
+      return this.url + z + "/" + x + "/" + y + "." + this.type;
+    } else {
+      return "http://www.maptiler.org/img/none.png";
+    }
   }
+
   options.projection = new OpenLayers.Projection(options.projection);
-  var layer = new OpenLayers.Layer.TMS(title, options.base_url, options);
-  return layer;
+  return new OpenLayers.Layer.TMS(title, options.base_url, options);
 };
