cb8e22906c1d7ece3ec586e8ca06210a20fd0b13
Augustus Kling <augustus.kling@geops.de>
2013-03-26 10:37:33 +0100

    Fix array iteration Do not longer rely on the presence of Object.keys as it is not available in IE8 Redraw layer initially to ensure it is hidden if resolution restrictions apply

--- plugins/behaviors/openlayers_behavior_layerzoom.js
+++ plugins/behaviors/openlayers_behavior_layerzoom.js
@@ -11,15 +11,27 @@ Drupal.openlayers.addBehavior('openlayers_behavior_layerzoom', function (data, o
   for (var layerName in options) {
     if (options[layerName].enable) {
       var layers = map.getLayersBy('drupalID', layerName);
-      // Go through selected layers to get full extent.
-      for (var i in layers) {
-        layer = layers[i];
-        // Restrict zoom levels.
-        var keys = Object.keys(options[layerName].resolutions);
+      jQuery(layers).each(function(index, layer){
+        // Basically replicate Object.keys because it is not available in IE8.
+        var keys = [];
+        for(var key in options[layerName].resolutions){
+          if(options[layerName].resolutions.hasOwnProperty(key)){
+            keys.push(key);
+          }
+        }
+        
+        // Restrict zoom level range.
         layer.alwaysInRange = false;
         layer.minResolution = options[layerName].resolutions[keys[keys.length-1]];
         layer.maxResolution = options[layerName].resolutions[keys[0]];
-      }
+        
+        if(layer.getVisibility() && layer.calculateInRange()===false){
+          // Ensure layer is redraw if it should be invisible initially due to
+          // the resolutions set above.
+          layer.setVisibility(false);
+          layer.setVisibility(true);
+        }
+      });
     }
   }
 });
