b7a73b4010a70a24e2a6fc371be90ae2531a0639
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-03-28 16:17:37 +0100

    Improve the Zoom To Layer behavior.

--- plugins/behaviors/openlayers_behavior_zoomtolayer.js
+++ plugins/behaviors/openlayers_behavior_zoomtolayer.js
@@ -8,7 +8,8 @@
  */
 Drupal.openlayers.addBehavior('openlayers_behavior_zoomtolayer', function (data, options) {
   var map = data.openlayers;
-  var zoomtolayer_scale = options.zoomtolayer_scale;
+  var zoomtolayer_scale = parseInt(options.zoomtolayer_scale, 10);
+  var point_zoom_level = parseInt(options.point_zoom_level, 10);
 
   var layers = map.getLayersBy('drupalID', {
     test: function(id) {
@@ -21,56 +22,72 @@ Drupal.openlayers.addBehavior('openlayers_behavior_zoomtolayer', function (data,
     }
   });
 
-  // Go through selected layers to get full extent.
-  map.fullExtent = new OpenLayers.Bounds();
+  // Combined extent of all layers.
+  var fullExtent = undefined;
+  // Number of layers that are still loading.
+  var pending_load_ends = 0;
 
+  // Go through selected layers to get full extent.
   jQuery(layers).each(function(index, layer) {
+    accumulate_extent(layer);
 
     if (layer instanceof OpenLayers.Layer.Vector) {
-
-      if (layer.getDataExtent() !== null) {
-        var layerextent = layer.getDataExtent().scale(zoomtolayer_scale);
-        map.fullExtent.extend(layerextent);
-        map.zoomToExtent(map.fullExtent);
-
-        // If unable to find width due to single point,
-        // zoom in with point_zoom_level option.
-        if (layerextent.getWidth() == 0.0) {
-          map.zoomTo(options.point_zoom_level);
-        }
-
-      } else {
-
-        layer.events.register('loadend', layer, function() {
-          var layerextent = layer.getDataExtent().scale(zoomtolayer_scale);
-          map.fullExtent.extend(layerextent);
-          map.zoomToExtent(map.fullExtent);
-
-          // If unable to find width due to single point,
-          // zoom in with point_zoom_level option.
-          if (layerextent.getWidth() == 0.0) {
-            map.zoomTo(options.point_zoom_level);
-          }
-        });
-
+      // This should not register the handler in case no data is available.
+      if (layer.getDataExtent() === null) {
+        // Try again to determine the extent after layer has loaded.
+        pending_load_ends = pending_load_ends + 1;
+        layer.events.register('loadend', layer, handle_loadend_once);
       }
 
-    } else {
-      var layerextent = layer.getDataExtent();
-      // Check for valid layer extent
-      if (layerextent != null) {
-        map.fullExtent.extend(layerextent);
-        map.zoomToExtent(map.fullExtent);
-      }
     }
   });
+  // Zoom if all data was sychronously load.
+  show_extent_if_determined();
 
-  // If unable to find width due to single point,
-  // zoom in with point_zoom_level option.
-  if (map.fullExtent.getWidth() == 0.0) {
-    map.zoomTo(options.point_zoom_level);
+  /**
+   * Handler for loadend event of layer that is still loading.
+   */
+  function handle_loadend_once(event) {
+    var layer = event.object;
+    layer.events.unregister('loadend', layer, handle_loadend_once);
+    pending_load_ends = pending_load_ends - 1;
+
+    accumulate_extent(layer);
+    // Zoom if no other layer is still loading.
+    show_extent_if_determined();
   }
-  else {
-    map.zoomToExtent(map.fullExtent.scale(zoomtolayer_scale));
+
+  /**
+   * Add data extent of layer to total data extent.
+   */
+  function accumulate_extent(layer){
+    var layerextent = layer.getDataExtent();
+    if(fullExtent instanceof OpenLayers.Bounds){
+      fullExtent.extend(layerextent);
+    } else {
+      fullExtent = layerextent;
+    }
+  }
+
+  /**
+   * Zooms map if all layers have finished loading.
+   */
+  function show_extent_if_determined(){
+    if(pending_load_ends===0 && fullExtent instanceof OpenLayers.Bounds){
+      if (fullExtent.getWidth()===0 && fullExtent.getHeight()===0) {
+        // If extent is a single point,
+        // zoom in with point_zoom_level option.
+        map.setCenter(fullExtent.getCenterLonLat(), point_zoom_level)
+      } else {
+        var scaled = fullExtent.scale(zoomtolayer_scale);
+        map.zoomToExtent(scaled);
+        if(!map.getExtent().contains(scaled)){
+          // OpenLayers silently ignores zoom in case the date line would need
+          // to be displayed more than once. Move map to where the data is at
+          // least.
+          map.setCenter(scaled.getCenterLonLat());
+        }
+      }
+    }
   }
 });
