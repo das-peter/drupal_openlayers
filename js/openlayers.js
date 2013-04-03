21862182400f06cb73fe413c2a8a86daeedbac77
Augustus Kling <augustus.kling@geops.de>
2013-04-03 09:22:37 +0200

    Remove maxExtent from layers as this setting is derived from the maps' projection bug #1942826

--- js/openlayers.js
+++ js/openlayers.js
@@ -57,7 +57,7 @@ Drupal.behaviors.openlayers = {
             options.projection = new OpenLayers.Projection(map.projection);
             options.displayProjection = new OpenLayers.Projection(map.displayProjection);
 
-            // Restrict map to its projection extent (data outwith cannot be represented)
+            // Restrict map to its extent (usually projection extent).
             options.maxExtent = new OpenLayers.Bounds.fromArray(map.maxExtent);
 
             options.maxResolution = 'auto'; // 1.40625;
