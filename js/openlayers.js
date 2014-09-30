d2afe2d7c8e2cc6f1f0b83c748c337ae12774f60
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2014-09-30 09:24:54 +0200

    Javascript small modifications.

--- js/openlayers.js
+++ js/openlayers.js
@@ -2,88 +2,79 @@
   Drupal.behaviors.openlayers = {
     'attach': function(context, settings) {
 
-      if (typeof(Drupal.settings.openlayers) === 'object' && Drupal.settings.openlayers.maps) {
-        $('.openlayers-map').once('openlayers-map', function() {
-          var map_id = $(this).attr('id');
-          if (map_id in Drupal.settings.openlayers.maps) {
-            try {
-              var object = Drupal.settings.openlayers.maps[map_id];
+      $('.openlayers-map').once('openlayers-map', function() {
+        var map_id = $(this).attr('id');
+        if (Drupal.settings.openlayers.maps[map_id] !== undefined) {
 
-              var layers = object.layer || [];
-              var controls = object.control || [];
-              var interactions = object.interaction || [];
-              var sources = object.source || [];
-              var components = object.component || [];
-              // Todo: see why this is not working with ajax.
-              var objects = {};
-              objects.sources = {};
-              objects.controls = {};
-              objects.interactions = {};
-              objects.components = {};
-              objects.layers = {};
-              objects.maps = {};
+          var object = Drupal.settings.openlayers.maps[map_id],
+            layers = object.layer || [],
+            controls = object.control || [],
+            interactions = object.interaction || [],
+            sources = object.source || [],
+            components = object.component || [],
+            objects = {sources: {}, controls: {}, interactions: {}, components: {}, layers: {}, maps: {}};
 
-              object.map.options.layers = [];
-              object.map.options.controls = [];
-              object.map.options.interactions = [];
-              object.map.options.components = [];
+          object.map.options.layers = [];
+          object.map.options.controls = [];
+          object.map.options.interactions = [];
+          object.map.options.components = [];
 
-              Drupal.openlayers.console.info("Creating map " + object.map.machine_name + "...");
-              var map = Drupal.openlayers[object.map['class']]({'map': object.map, 'context': context});
-              objects.maps[map_id] = map;
-              Drupal.openlayers.console.info("Creating map object... done !");
+          try {
+            Drupal.openlayers.console.info("Creating map " + object.map.machine_name + "...");
+            var map = Drupal.openlayers[object.map['class']]({'map': object.map, 'context': context});
+            objects.maps[map_id] = map;
+            Drupal.openlayers.console.info("Creating map object... done !");
 
-              Drupal.openlayers.console.info("Building sources...");
-              sources.map(function(data) {
-                objects.sources[data.machine_name] = Drupal.openlayers.getObject(context, 'sources', data, map);
-              });
-              Drupal.openlayers.console.info("Building sources... done !");
+            Drupal.openlayers.console.info("Building sources...");
+            sources.map(function(data) {
+              objects.sources[data.machine_name] = Drupal.openlayers.getObject(context, 'sources', data, map);
+            });
+            Drupal.openlayers.console.info("Building sources... done !");
 
-              Drupal.openlayers.console.info("Building controls...");
-              controls.map(function(data) {
-                map.addControl(Drupal.openlayers.getObject(context, 'controls', data, map));
-              });
-              Drupal.openlayers.console.info("Building controls... done !");
+            Drupal.openlayers.console.info("Building controls...");
+            controls.map(function(data) {
+              map.addControl(Drupal.openlayers.getObject(context, 'controls', data, map));
+            });
+            Drupal.openlayers.console.info("Building controls... done !");
 
-              Drupal.openlayers.console.info("Building interactions...");
-              interactions.map(function(data) {
-                objects.interactions[data.machine_name] = Drupal.openlayers.getObject(context, 'interactions', data, map);
-                map.addInteraction(objects.interactions[data.machine_name]);
-              });
-              Drupal.openlayers.console.info("Building interactions... done !");
+            Drupal.openlayers.console.info("Building interactions...");
+            interactions.map(function(data) {
+              objects.interactions[data.machine_name] = Drupal.openlayers.getObject(context, 'interactions', data, map);
+              map.addInteraction(objects.interactions[data.machine_name]);
+            });
+            Drupal.openlayers.console.info("Building interactions... done !");
 
-              Drupal.openlayers.console.info("Building layers...");
-              layers.map(function(data) {
-                Drupal.openlayers.console.info(" Adding source to layer...");
-                data.options.source = objects.sources[data.options.source];
-                objects.layers[data.machine_name] = Drupal.openlayers.getObject(context, 'layers', data, map);
-                Drupal.openlayers.console.info(" Adding layer to map...");
-                map.addLayer(objects.layers[data.machine_name]);
-              });
-              Drupal.openlayers.console.info("Building layers... done !");
+            Drupal.openlayers.console.info("Building layers...");
+            layers.map(function(data) {
+              Drupal.openlayers.console.info(" Adding source to layer...");
+              data.options.source = objects.sources[data.options.source];
+              objects.layers[data.machine_name] = Drupal.openlayers.getObject(context, 'layers', data, map);
+              Drupal.openlayers.console.info(" Adding layer to map...");
+              map.addLayer(objects.layers[data.machine_name]);
+            });
+            Drupal.openlayers.console.info("Building layers... done !");
 
-              Drupal.openlayers.console.info("Building components...");
-              components.map(function(data) {
-                objects.components[data.machine_name] = Drupal.openlayers.getObject(context, 'components', data, map);
-              });
-              Drupal.openlayers.console.info("Building components... done !");
+            Drupal.openlayers.console.info("Building components...");
+            components.map(function(data) {
+              objects.components[data.machine_name] = Drupal.openlayers.getObject(context, 'components', data, map);
+            });
+            Drupal.openlayers.console.info("Building components... done !");
 
-              // Attach data to map DOM object
-              Drupal.openlayers.console.info("Caching objects...");
-              jQuery(context).data('openlayers', {'objects': objects});
-              Drupal.openlayers.console.info("Caching objects... done !");
+            // Attach data to map DOM object
+            Drupal.openlayers.console.info("Caching objects...");
+            jQuery(context).data('openlayers', {'objects': objects});
+            Drupal.openlayers.console.info("Caching objects... done !");
 
-            } catch (e) {
-              var errorMessage = e.name + ': ' + e.message;
-              if (typeof console !== 'undefined') {
-                Drupal.openlayers.console.log(errorMessage);
-              } else {
-                $(this).text('Error during map rendering: ' + errorMessage);
-              }
+          } catch (e) {
+            var errorMessage = e.name + ': ' + e.message;
+            if (typeof console !== 'undefined') {
+              Drupal.openlayers.console.log(errorMessage);
+            } else {
+              $(this).text('Error during map rendering: ' + errorMessage);
             }
           }
-        });
-      }
+        }
+      });
     }
   };
 
@@ -139,10 +130,7 @@
           // Ignore call as no logging facility is available.
         };
       }
-      jQuery(["log", "debug", "info", "warn", "exception", "assert", "dir",
-        "dirxml", "trace", "group", "groupEnd", "groupCollapsed", "profile",
-        "profileEnd", "count", "clear", "time", "timeEnd", "timeStamp", "table",
-        "error"]).each(function(index, functionName){
+      jQuery(["log", "debug", "info", "warn", "exception", "assert", "dir","dirxml", "trace", "group", "groupEnd", "groupCollapsed", "profile","profileEnd", "count", "clear", "time", "timeEnd", "timeStamp", "table","error"]).each(function(index, functionName){
         if (typeof(console)!=="object" || typeof(console[functionName])!=="function"){
           // Use fallback as browser does not provide implementation.
           api[functionName] = logger;
