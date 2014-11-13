c1949222e1169e50e7f887b2beef3d6f2377f15e
Peter Philipp <peter.philipp@cando-image.com>
2014-11-13 10:02:33 +0100

    Enhance google maps integration by providing a way of fetching the Google Maps object.

--- plugins/openlayers/source/openlayers__source__googlemaps.js
+++ plugins/openlayers/source/openlayers__source__googlemaps.js
@@ -3,7 +3,7 @@ Drupal.openlayers.openlayers__source__googlemaps = function(data) {
   var olMapDiv = jQuery(data.map.getViewport()).parent();
   var gmapDiv = jQuery('#gmap-' + olMapDiv.attr('id'));
 
-  data.options.gmap = new google.maps.Map(gmapDiv[0], {
+  var gmap = new google.maps.Map(gmapDiv[0], {
     disableDefaultUI: true,
     keyboardShortcuts: false,
     draggable: false,
@@ -12,20 +12,29 @@ Drupal.openlayers.openlayers__source__googlemaps = function(data) {
     streetViewControl: false,
     mapTypeId: google.maps.MapTypeId[data.options.mapTypeId] || 'roadmap'
   });
+  gmapDiv.data('gmap', gmap);
 
   data.map.getView().on('change:center', function() {
     var center = ol.proj.transform(data.map.getView().getCenter(), 'EPSG:3857', 'EPSG:4326');
-    data.options.gmap.setCenter(new google.maps.LatLng(center[1], center[0]));
+    gmap.setCenter(new google.maps.LatLng(center[1], center[0]));
   });
   data.map.getView().on('change:resolution', function() {
-    data.options.gmap.setZoom(data.map.getView().getZoom());
+    gmap.setZoom(data.map.getView().getZoom());
   });
 
   data.map.getView().setCenter(data.map.getView().getCenter());
   data.map.getView().setZoom(data.map.getView().getZoom());
 
   olMapDiv[0].parentNode.removeChild(olMapDiv[0]);
-  data.options.gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv[0]);
+  gmap.controls[google.maps.ControlPosition.TOP_LEFT].push(olMapDiv[0]);
 
   return new ol.source.Vector();
 };
+
+/**
+ * Helper to access the gmap instance in a ol.Map.
+ */
+Drupal.openlayers.openlayers__source__googlemaps_get_map = function(map){
+  var olMapDiv = jQuery(map.getViewport()).parent();
+  return jQuery('#gmap-' + olMapDiv.attr('id')).data('gmap');
+}
