713347c54390a0297fd6964c59d79de4dfbe7ac1
Peter Philipp <peter.philipp@cando-image.com>
2014-11-13 08:29:16 +0100

    Make vector source work with multiple features. Properly inject attributes.

--- plugins/openlayers/source/openlayers__source__vector.js
+++ plugins/openlayers/source/openlayers__source__vector.js
@@ -1,12 +1,23 @@
 Drupal.openlayers.openlayers__source__vector = function(data) {
 
-  var format = new ol.format.WKT();
-  var feature = format.readFeature(data.options.features);
-  feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
-
+  // Ensure the features are really an array.
+  if (!(data.options.features instanceof Array)) {
+    data.options.features = [{geometry: data.options.features}];
+  }
   var options = {
-    features: [feature]
+    features: []
   };
-
+  var format = new ol.format.WKT();
+  for(var i in data.options.features) {
+    var data_projection = data.options.features[i].projection || 'EPSG:4326';
+    var feature = format.readFeature(data.options.features[i].wkt, {
+      dataProjection: data_projection,
+      featureProjection: data.map.getView().getProjection()
+    });
+    if (goog.isDef(data.options.features[i].attributes)) {
+      feature.setProperties(data.options.features[i].attributes);
+    }
+    options.features.push(feature);
+  }
   return new ol.source.Vector(options);
 };
