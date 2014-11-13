80fd664dc3f919c2e607a18cf0bd846a16b0ec87
Peter Philipp <peter.philipp@cando-image.com>
2014-11-13 09:00:56 +0100

    More stable JS code for vector source.

--- plugins/openlayers/source/openlayers__source__vector.js
+++ plugins/openlayers/source/openlayers__source__vector.js
@@ -1,23 +1,25 @@
 Drupal.openlayers.openlayers__source__vector = function(data) {
 
-  // Ensure the features are really an array.
-  if (!(data.options.features instanceof Array)) {
-    data.options.features = [{geometry: data.options.features}];
-  }
   var options = {
     features: []
   };
-  var format = new ol.format.WKT();
-  for(var i in data.options.features) {
-    var data_projection = data.options.features[i].projection || 'EPSG:4326';
-    var feature = format.readFeature(data.options.features[i].wkt, {
-      dataProjection: data_projection,
-      featureProjection: data.map.getView().getProjection()
-    });
-    if (goog.isDef(data.options.features[i].attributes)) {
-      feature.setProperties(data.options.features[i].attributes);
+  if (goog.isDef(data.options.features)) {
+    // Ensure the features are really an array.
+    if (!(data.options.features instanceof Array)) {
+      data.options.features = [{geometry: data.options.features}];
     }
-    options.features.push(feature);
+    var format = new ol.format.WKT();
+    for (var i in data.options.features) {
+      var data_projection = data.options.features[i].projection || 'EPSG:4326';
+      var feature = format.readFeature(data.options.features[i].wkt, {
+        dataProjection: data_projection,
+        featureProjection: data.map.getView().getProjection()
+      });
+      if (goog.isDef(data.options.features[i].attributes)) {
+        feature.setProperties(data.options.features[i].attributes);
+      }
+      options.features.push(feature);
+    }
   }
   return new ol.source.Vector(options);
 };
